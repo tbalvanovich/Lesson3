e0274d7e371cacfe89cf0c31820ceef1e14c5d1b
tbalvanovich <78137299+tbalvanovich@users.noreply.github.com>
2021-02-08 18:50:52 +0300

    Update Main.java
    Add correct code

--- src/by/balvanovich/lesson3/Task6/Main.java
+++ src/by/balvanovich/lesson3/Task6/Main.java
@@ -4,11 +4,10 @@ public class Main {
     public static void main(String[] args) {
         double s = 10;
         double sum = 0;
+        double pr = 1.1;
         for (int i = 1; i <=7; i++){
             sum = sum + s;
-            s = s * 1.1;
-
-
+            s = s * pr;
         }
         System.out.println(sum);
     }
