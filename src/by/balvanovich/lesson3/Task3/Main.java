d3df109007db8869ae6268a213fa3ac8c55db08b
tbalvanovich <78137299+tbalvanovich@users.noreply.github.com>
2021-02-08 18:47:46 +0300

    Update Main.java
    Add correct code

--- src/by/balvanovich/lesson3/Task3/Main.java
+++ src/by/balvanovich/lesson3/Task3/Main.java
@@ -3,7 +3,7 @@ package by.balvanovich.lesson3.Task3;
 public class Main {
     public static void main(String[] args) {
         int sum = 0;
-        for (int i = 0; i < 257; i++){
+        for (int i = 1; i < 257; i = i * 2){
             sum = i +sum;
         }
         System.out.println(sum);
