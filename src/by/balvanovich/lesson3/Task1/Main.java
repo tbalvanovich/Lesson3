6b27b900555fb5d9989e4d079979910485cb8560
tbalvanovich <78137299+tbalvanovich@users.noreply.github.com>
2021-02-08 18:41:16 +0300

    Update Main.java
    Add new code

--- src/by/balvanovich/lesson3/Task1/Main.java
+++ src/by/balvanovich/lesson3/Task1/Main.java
@@ -3,7 +3,7 @@ package by.balvanovich.lesson3.Task1;
 public class Main {
     public static void main(String[] args) {
         int arr[] = {1,2,5,4567,87,65,45};
-        int max = arr[0];
+        int max = Integer.MIN_VALUE;
         for (int i = 1; i < arr.length; i++){
             if (arr[i] > max) {
                 max = arr[i];
