fb52861273a770ea45b015872c2b65e025057c4b
tbalvanovich <78137299+tbalvanovich@users.noreply.github.com>
2021-02-08 19:09:59 +0300

    Update Main.java
    Add new code

--- src/by/balvanovich/lesson3/Task7/Main.java
+++ src/by/balvanovich/lesson3/Task7/Main.java
@@ -4,17 +4,30 @@ public class Main {
     public static void main(String[] args) {
         int arr[] = {300, 30, 21, 15, 7, 43, 1};
         int temp = 0;
-        for ( int a = 1; a < arr.length; a++)
-        for (int i = 0; i < arr.length - a; i++){
-            if (arr[i] > arr[i + 1]) {
-                    temp = arr[i];
-                    arr[i] = arr[i + 1];
-                    arr[i + 1] = temp;
+        int indexMin = 0, indexMax = 0;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
 
+        for (int i = 0; i < arr.length; i++){
+
+            if (arr[i] > max){
+                max = arr[i];
+                indexMax = i;
             }
+            if (arr[i] < min){
+
+                min = arr[i];
+                indexMin = i;
+            }
+            }
+        if (indexMin > indexMax){
+            temp = indexMax;
+            indexMax = indexMin;
+            indexMin = temp;
         }
+
         int sum = 0;
-        for (int i = 1; i < arr.length - 1; i++)
+        for (int i = indexMin + 1; i < indexMax; i++)
             sum = sum + arr[i];
         System.out.println(sum);
     }
